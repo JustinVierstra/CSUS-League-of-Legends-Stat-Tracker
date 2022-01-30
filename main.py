7943e728192dd766f34b9fc3649a09ae3ac74eeb
jjrv360 <jjrv360@gmail.com>
2022-01-29 19:50:22 -0800

    More Features
    Added bans, kill participation, and gold earned

--- main.py
+++ main.py
@@ -26,6 +26,14 @@ puuid = response.json()['puuid']
 
 matches = requests.get(f"https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?queue={queue}&count=10&api_key={API_KEY}")
 
+championList = requests.get("http://ddragon.leagueoflegends.com/cdn/12.2.1/data/en_US/champion.json")
+crd = championList.json()['data']
+
+def getChampName(id):
+    for i in crd:
+        if(crd[i]["key"] == str(id)):
+            return i
+
 print("Response: " + str(response))
 print("Matches: " + str(matches))
 
@@ -33,7 +41,6 @@ possibleNewGames = []
 possibleNewGames = matches.json()
 pastGames = []
 actuallyNew = []
-
 
 exists = os.path.exists('gameList.txt')
 
@@ -69,17 +76,38 @@ def firstBlood(first_blood):
         return 1
     return 0
 
+def kp(kills, team):
+    if(team<5):
+        return str(round(kills/side1TotalKills*100, 2)) + "%"
+    else:
+        return str(round(kills/side2TotalKills*100, 2)) + "%"
+
 allStats = []
 for game in actuallyNew:
     matchData = requests.get(f"https://americas.api.riotgames.com/lol/match/v5/matches/{game}?api_key={API_KEY}")
     print(game + " added to spreadsheet")
-    allStats.append(["Name", "Champion", "Kills", "Deaths", "Assists", "KDA", "CS", "Damage", "First Blood", "Vision Score", "Wards Placed", "Wards Killed", "Pink Wards", "Role", "Team ID", "Game Length", "Game Result", "Side"])
+    allStats.append(["Player Name:", "Champion:", "Kills:", "Deaths:", "Assists:", "KDA:", "KP:", "CS:", "Damage:", "Gold Earned:", "First Blood:", "Vision Score:", "Wards Placed:", "Wards Killed:", "Pink Wards:", "Role:", "Team ID:", "Game Length:", "Game Result:", "Side:", "", "Bans:"])
+    side1TotalKills = 0
+    side2TotalKills = 0
+
+    for i in range(0,10):
+        playerData = matchData.json()['info']['participants'][i]
+
+        if(i<5):
+            side1TotalKills += playerData['kills']
+        elif(i<10):
+            side2TotalKills += playerData['kills']
+
     for i in range(0, 10):
         playerData = matchData.json()['info']['participants'][i]
+        if(i<5):
+            banData = matchData.json()['info']['teams'][0]['bans'][i]['championId']
+        else:
+            banData = matchData.json()['info']['teams'][1]['bans'][i%5]['championId']
         row = [playerData['summonerName'], playerData['championName'], playerData['kills'],
-        playerData['deaths'], playerData['assists'], kda(playerData['kills'], playerData['deaths'], playerData['assists']), playerData['totalMinionsKilled'], 
-        playerData['totalDamageDealtToChampions'], firstBlood(playerData['firstBloodKill']), playerData['visionScore'], 
-        playerData['wardsPlaced'], playerData['wardsKilled'], playerData['visionWardsBoughtInGame'], playerData['lane'], 0, matchData.json()['info']['gameDuration'], gameResult(playerData['win']), playerData['teamId']]
+        playerData['deaths'], playerData['assists'], kda(playerData['kills'], playerData['deaths'], playerData['assists']), kp(playerData['kills'], i),playerData['totalMinionsKilled'], 
+        playerData['totalDamageDealtToChampions'], playerData['goldEarned'], firstBlood(playerData['firstBloodKill']), playerData['visionScore'], 
+        playerData['wardsPlaced'], playerData['wardsKilled'], playerData['visionWardsBoughtInGame'], playerData['lane'], 0, matchData.json()['info']['gameDuration'], gameResult(playerData['win']), playerData['teamId'], "", getChampName(banData)]
         allStats.append(row)
 
 with open("stats.csv", "a", newline='') as f:
